76b9fb2ba5d98f679da91afc3b47ccd7e000c62c
sapna2311 <itzsanasharma1418@gmail.com>
2024-07-24 23:32:01 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,4 @@
-// import logo from "../assets/aboutt.png";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+
 import { motion } from 'framer-motion';
 
 const Navbar = () => {
